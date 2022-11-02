449015281b9cf266ea2160849d26d91bd0029779
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-11-02 15:37:09 +0500

    code updated

---
+++ Lecture01Variablesanddatatype.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("salam alikum \n");
+  /*
+   Szic Classes
+
+  */
+
+  // declaration
+  int a;
+  // intialization
+  a = 5;
+
+  /*
+int num1=5;
+int num2=5.5;
+int numadd=num1+num2;
+int numsub=num1-num2;
+int nummul=num1*num2;
+int numdiv=num1/num2;
+
+
+
+  printf("%d\n",numadd);
+  printf("%d\n",numsub);
+  printf("%d\n",nummul);
+  printf("%d\n",numdiv);
+
+   */
+  /*
+
+double num1=5;
+double num2=5.5;
+double numadd=num1+num2;
+double numsub=num1-num2;
+double nummul=num1*num2;
+double numdiv=num1/num2;
+
+ printf("%lf\n",numadd);
+ printf("%lf\n",numsub);
+ printf("%lf\n",nummul);
+ printf("%lf\n",numdiv);
+
+*/
+
+  float num1 = 5.5;
+  float num2 = 5.5;
+  float numadd = num1 + num2;
+  float numsub = num1 - num2;
+  float nummul = num1 * num2;
+  float numdiv = num1 / num2;
+
+  printf("%f\n", numadd);
+  printf("%f\n", numsub);
+  printf("%f\n", nummul);
+  printf("%f\n", numdiv);
+
+  return 0;
+}
---
+++ Lecture03Inputtype.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num1;
+    scanf("%d", &num1);
+
+    int num2;
+    scanf("%d", &num2);
+
+    int add = num1 + num2;
+
+    int sub = num1 - num2;
+    
+
+    printf("by adding answer %d \n", add);
+    printf("app ne jab minus kya answer  %d \n", sub);
+    return 0;
+}
